7e4b43ef987e0f8fbab04c8c065f6d9ff87cfd71
muhammad-di <dzhabrailov3.muhammad@yandex.ru>
2023-12-02 07:54:05 +0300

    diplom first part 1.1

--- ewm-service/src/test/java/ru/practicum/MainServiceTest.java
+++ ewm-service/src/test/java/ru/practicum/MainServiceTest.java
@@ -6,5 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class MainServiceTest {
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+    }
 }
